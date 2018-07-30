25d7d0564a72fc53b58fb85f3988a63f026dfebc
Shasha Hua <32149495+shashahua@users.noreply.github.com>
2018-07-29 19:33:09 -0700

    Add files via upload

---
+++ app/src/main/java/com/example/shasha/MapFragment.java
@@ -0,0 +1,24 @@
+package com.example.shasha.navigationmenu;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * Created by shasha on 2018-07-28.
+ *
+ */
+
+public class MapFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_map, container, false);
+    }
+}
